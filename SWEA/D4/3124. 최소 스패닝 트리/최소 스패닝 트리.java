d71e416a8cc56dc078f7924054116afda84c5761
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2024-08-30 14:25:40 +0900

    [D4] Title: 최소 스패닝 트리, Time: 7,464 ms, Memory: 307,436 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,81 +1,73 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
+    static int v, e;
+    static List<Edge>[] edges;
+    static PriorityQueue<Edge> q;
 
-    static int V, T;
-    static int[] parents;
+    static class Edge {
+        int x,y;
 
-    static void make() {
-        for (int i = 0; i < V; i++) {
-            parents[i] = -1;
+        public Edge(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-    }
-
-    static int findSet(int a) {
-        if (parents[a] < 0) return a;
-        return parents[a] = findSet(parents[a]);  // 경로 압축
-    }
-
-    static boolean union(int a, int b) {
-        int aRoot = findSet(a);
-        int bRoot = findSet(b);
-        if (aRoot == bRoot) return false;
-
-        if (parents[aRoot] < parents[bRoot]) {  // Union by size
-            parents[aRoot] += parents[bRoot];
-            parents[bRoot] = aRoot;
-        } else {
-            parents[bRoot] += parents[aRoot];
-            parents[aRoot] = bRoot;
-        }
-        return true;
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        T = sc.nextInt();
-        for (int tc = 1; tc <= T; tc++) {
-            V = sc.nextInt();
-            int E = sc.nextInt();
 
-            parents = new int[V+1];
-            Edge[] edges = new Edge[E];
-            for (int i = 0; i < E; i++) {
-                edges[i] = new Edge(sc.nextInt(), sc.nextInt(), sc.nextLong());  // weight를 long으로 변경
+        int T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            v = sc.nextInt();
+            e = sc.nextInt();
+            edges = new ArrayList[v + 1];
+            for (int i = 1; i <= v; i++) {
+                edges[i] = new ArrayList<>();
             }
 
-            Arrays.sort(edges);
-            make();
-            int cnt = 0;
-            long cost = 0;  // 비용을 long으로 변경
+            //최소 힙 비교
+            q = new PriorityQueue<>(new Comparator<Edge>() {
+                @Override
+                public int compare(Edge e1, Edge e2) {
+                    return Integer.compare(e1.y, e2.y); 
+                }
+            });
 
-            for (Edge edge : edges) {
-                if (union(edge.start, edge.end)) {
-                    cnt++;
-                    cost += edge.weight;
-                    if (cnt == V - 1) break;
+            for (int i = 0; i < e; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                int w = sc.nextInt();
+
+                edges[x].add(new Edge(y, w)); // x에서 y로 가는 엣지
+                edges[y].add(new Edge(x, w)); // y에서 x로 가는 엣지 (무향 그래프)
+            }
+
+            boolean[] visit = new boolean[v + 1];
+            q.offer(new Edge(1, 0)); 
+
+            long ans = 0;
+            
+            
+            while (!q.isEmpty()) {
+                Edge edge = q.poll();
+                int currentVertex = edge.x;
+                int cost = edge.y;
+
+                if (visit[currentVertex]) continue;
+
+                visit[currentVertex] = true;
+                ans += cost;
+
+                for (Edge e : edges[currentVertex]) {
+                    if (!visit[e.x]) {
+                        q.offer(e);
+                    }
                 }
             }
-            System.out.println("#" + tc + " " + cost);
-        }
-        sc.close();
-    }
-
-    static class Edge implements Comparable<Edge> {
-        int start, end;
-        long weight;  // weight를 long으로 변경
-
-        public Edge(int start, int end, long weight) {
-            this.start = start;
-            this.end = end;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Edge o) {
-            return Long.compare(this.weight, o.weight);  // long 비교로 수정
+            
+            System.out.println("#" + tc + " " + ans);
         }
     }
 }
