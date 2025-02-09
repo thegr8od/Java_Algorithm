7d77987ecb1b63c95bc0177ef9db7fdd483e28b1
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-02-09 17:54:42 +0900

    [Silver V] Title: 돌 게임, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+    static int ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        if(n%4==1 || n%4==3){
+            System.out.println("SK");
+        }
+        else{
+            System.out.println("CY");
+        }
+    }
+}
