eeb93189614ad11693639517106af643135b1981
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-04-22 15:00:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.33 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    
+    int[] dx = {0,1,-1,0};
+    int[] dy = {1,0,0,-1};
+    
+    public int solution(int[][] maps) {
+        int answer = 0;
+        
+        int[][] visited = new int[maps.length][maps[0].length];
+        
+        bfs(maps, visited);
+        answer = visited[maps.length-1][maps[0].length-1];
+        
+        if (answer == 0){
+            answer = -1;
+        }
+        
+        return answer;
+    }
+    
+    public void bfs(int[][] maps, int[][] visited){
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0});
+        visited[0][0] = 1;
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+
+            for(int i =0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx < 0 || nx > maps.length -1 || ny <0 || ny >maps[0].length-1)
+                    continue;
+                
+                if (visited[nx][ny] == 0 && maps[nx][ny] == 1){
+                    visited[nx][ny] = visited[x][y] + 1;
+                    queue.add(new int[]{nx,ny});
+                }
+            }
+            
+        }
+    }
+}
