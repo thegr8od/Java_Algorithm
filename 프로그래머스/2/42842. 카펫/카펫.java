5ac70ff204dd9f4ace760e11dea7e8a575ed6510
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-04-23 15:03:16 +0900

    [level 2] Title: 카펫, Time: 9.64 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int allCnt = brown + yellow;
+        
+        for (int i =3; i<allCnt; i++){
+            int row = i;
+            int col = allCnt / row;
+            
+            if(row>=col){
+                if((row-2) * (col-2) == yellow){
+                    answer[0] = row;
+                    answer[1] = col;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
