db8eed79ab70abf88d24a72b0d95d255a83415e7
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-04-26 22:09:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.07 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,51 +2,49 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 9999999;
+        List<List<Integer>> lst = new ArrayList<>();
         
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i=0; i<=n; i++){
-            graph.add(new ArrayList<>());
+        for(int i=0; i<=n; i++){
+            lst.add(new ArrayList<>());
         }
         
         for (int[] wire : wires){
             int a = wire[0];
             int b = wire[1];
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            lst.get(a).add(b);
+            lst.get(b).add(a);
         }
         
-        for(int[] wire : wires){
+        for (int[] wire : wires){
             int a = wire[0];
             int b = wire[1];
-            graph.get(a).remove(Integer.valueOf(b));
-            graph.get(b).remove(Integer.valueOf(a));
-            
+            lst.get(a).remove(Integer.valueOf(b));
+            lst.get(b).remove(Integer.valueOf(a));
             boolean[] visited = new boolean[n+1];
-            int countA = dfs(a, graph, visited);
-            int countB = n - countA;
+            int first = dfs(a, lst, visited);
+            int second = n - first;
             
-            answer = Math.min(answer, Math.abs(countA-countB));
-            
-            graph.get(a).add(b);
-            graph.get(b).add(a);
+            answer = Math.min(answer, Math.abs(first-second));
+            lst.get(a).add(b);
+            lst.get(b).add(a);
         }
-        
         
         return answer;
     }
     
-    public int dfs(int node, List<List<Integer>> graph, boolean[] visited){
-    
-        visited[node] = true;
-        int count = 1;
+    public int dfs(int idx, List<List<Integer>> lst, boolean[] visited){
+        visited[idx] = true;
+        int cnt = 1;
         
-        for(int next : graph.get(node)){
-            if (!visited[next]){
-                count += dfs(next, graph, visited);
+        
+        for(int next : lst.get(idx)){
+            if(!visited[next]){
+                cnt += dfs(next,lst,visited);
             }
         }
         
-        return count;
+        
+        return cnt;
     }
 }
