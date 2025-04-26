8777e6e471298750949c884abd10b6ae5bcee2e8
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-04-26 21:26:58 +0900

    [level 2] Title: 소수 찾기, Time: 23.08 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> set;
-    static boolean[] visited = new boolean[7];
-    
-    
+    static boolean[] visited;
+    static HashSet<Integer> set;
     public int solution(String numbers) {
         int answer = 0;
+        visited = new boolean[numbers.length()];
         set = new HashSet<>();
-        dfs(numbers, "", 0);
-        
-        for(Integer num : set){
-            if(isPrime(num)){
-                answer++;
-            }
+        dfs(numbers,"",0);
+        for(int n : set){
+            if(isPrime(n)) answer++;
         }
         return answer;
     }
     
     public static void dfs(String numbers, String s, int depth){
+        
         if(depth > numbers.length()){
             return;
         }
-        
-        for (int i =0; i< numbers.length(); i++){
+        for(int i=0; i<numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                set.add(Integer.parseInt(s + numbers.charAt(i)));
+                set.add(Integer.parseInt(s+numbers.charAt(i)));
                 dfs(numbers, s + numbers.charAt(i), depth+1);
                 visited[i] = false;
             }
+            
         }
     }
     
-    public static boolean isPrime(int n) {
-        if (n < 2){
+    public static boolean isPrime(int n){
+        if (n<2){
             return false;
         }
         
-        for (int i =2; i<= (int) Math.sqrt(n); i++){
+        for(int i=2; i<=(int)Math.sqrt(n); i++){
             if(n%i == 0) {
                 return false;
             }
