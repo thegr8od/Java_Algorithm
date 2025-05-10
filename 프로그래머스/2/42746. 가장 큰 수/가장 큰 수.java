487252c77795cc02433b64f98bc0208b8570c277
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-05-10 15:51:00 +0900

    [level 2] Title: 가장 큰 수, Time: 171.33 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] arr = new String[numbers.length];
         
-        for (int i =0; i< arr.length; i++){
-            arr[i] = String.valueOf(numbers[i]);
+        String[] data = new String[numbers.length];
+        
+        for(int i=0; i<data.length; i++){
+            data[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(arr, (o1,o2) -> (o2 + o1).compareTo(o1 + o2));
+        Arrays.sort(data, (o1,o2) -> (o2+o1).compareTo(o1+o2));
         
-        if(arr[0].equals("0")){
-            return "0";
+        if(data[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String str : data){
+            sb.append(str);
         }
         
-        StringBuilder answer = new StringBuilder();
         
-        for (int i =0; i<arr.length; i++){
-            answer.append(arr[i]);
-        }
+        return sb.toString();
         
-        return answer.toString();
     }
 }
-
