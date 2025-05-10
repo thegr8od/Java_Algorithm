7a4e03b43d5f2ba390a7c227e10067aa286f3b38
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-05-10 23:31:56 +0900

    [level 3] Title: 가장 먼 노드, Time: 30.48 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,6 +1,48 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        List<Integer>[] lst = new ArrayList[n+1];
+        boolean[] visited = new boolean[n+1];
+        int[] arr = new int[n+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for(int i=0; i<n+1; i++){
+            lst[i] = new ArrayList<>();
+        }
+        
+        for(int i=0; i<edge.length; i++){
+            int a = edge[i][0];
+            int b = edge[i][1];
+            lst[a].add(b);
+            lst[b].add(a);
+        }
+        
+        queue.add(1);
+        visited[1] = true;
+        
+        while(!queue.isEmpty()){
+            int num = queue.poll();
+            for(int next : lst[num]){
+                if(!visited[next]){
+                    queue.add(next);
+                    visited[next] = true;
+                    arr[next] = arr[num] + 1;
+                }
+            }
+        }
+        
+        Arrays.sort(arr);
+        int max = arr[n];
+        
+        for(int num : arr){
+            if(num == max){
+                answer++;
+            }
+        }
+        
+        
         return answer;
     }
 }
