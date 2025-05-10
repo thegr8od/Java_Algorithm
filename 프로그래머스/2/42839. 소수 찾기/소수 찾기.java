e327c40df9df76cd75c4d0154db97b1f91d73195
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-05-10 16:54:23 +0900

    [level 2] Title: 소수 찾기, Time: 33.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,44 +3,59 @@ import java.util.*;
 class Solution {
     static boolean[] visited;
     static HashSet<Integer> set;
+    static int len;
     public int solution(String numbers) {
         int answer = 0;
-        visited = new boolean[numbers.length()];
+        len = numbers.length();
+        visited = new boolean[len];
         set = new HashSet<>();
-        dfs(numbers,"",0);
-        for(int n : set){
-            if(isPrime(n)) answer++;
+        dfs("",0,numbers);
+
+        
+        for(int num : set){
+            if(isPrime(num)) answer++;
+        }
+        
+        for(int num : set){
+            System.out.println(num);
         }
         return answer;
+        
     }
     
-    public static void dfs(String numbers, String s, int depth){
+    public static void dfs(String str, int depth, String numbers){
         
-        if(depth > numbers.length()){
+        if(depth > len){
             return;
         }
-        for(int i=0; i<numbers.length(); i++){
+        
+        for(int i=0; i<len; i++){
             if(!visited[i]){
                 visited[i] = true;
-                set.add(Integer.parseInt(s+numbers.charAt(i)));
-                dfs(numbers, s + numbers.charAt(i), depth+1);
+                String now = str + numbers.charAt(i);
+                set.add(Integer.parseInt(now));
+                dfs(now, depth+1, numbers);
                 visited[i] = false;
             }
-            
-        }
-    }
-    
-    public static boolean isPrime(int n){
-        if (n<2){
-            return false;
         }
         
+    }
+    
+    
+    
+    public static boolean isPrime(int n){
+        if(n<2){
+            return false;
+        }    
+        
         for(int i=2; i<=(int)Math.sqrt(n); i++){
-            if(n%i == 0) {
+            if(n%i == 0){
                 return false;
             }
         }
         
         return true;
+        
     }
+    
 }
