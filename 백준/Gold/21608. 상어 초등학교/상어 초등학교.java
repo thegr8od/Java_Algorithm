f1678c2eda96e7fdb6b540ef6e5f15336aa6de7e
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-05-21 23:41:22 +0900

    [Gold V] Title: 상어 초등학교, Time: 228 ms, Memory: 20036 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.java
@@ -2,184 +2,124 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int N;
-    static List<int[]> order1;
-    static List<int[]> order2;
-    static int[][] num;
     static int[][] map;
     static int[][] data;
     static int[] order;
     static int answer;
-    public static void main (String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        int times = N*N;
-        data = new int[times+1][4];
-        map = new int[N+1][N+1];
-        order = new int[times+1];
-        for(int i=0; i<times; i++){
+        int total = N * N;
+
+        map = new int[N + 1][N + 1];
+        data = new int[total + 1][4];
+        order = new int[total];
+
+        for (int i = 0; i < total; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int now = Integer.parseInt(st.nextToken());
-            order[i] = now;
-            for(int j=0; j<4; j++){
-                int like = Integer.parseInt(st.nextToken());
-                data[now][j] = like;
+            int student = Integer.parseInt(st.nextToken());
+            order[i] = student;
+            for (int j = 0; j < 4; j++) {
+                data[student][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-
-        for(int n : order){
-            order1 = new ArrayList<>();
-            findLike(n);
-            if(order1.size()==1){
-                int[] now = order1.get(0);
-                int x = now[0];
-                int y = now[1];
-                map[x][y] = n;
-            }
-            else {
-                order2 = new ArrayList<>();
-                emptyNum();
-                if(order2.size()>0){
-                    int now[] = order2.get(0);
-                    int x = now[0];
-                    int y = now[1];
-                    map[x][y] = n;
-                }
-
-            }
+        for (int student : order) {
+            List<int[]> candidates = findLike(student);
+            List<int[]> result = (candidates.size() == 1) ? candidates : findMostEmpty(candidates);
+            int[] pos = result.get(0);
+            map[pos[0]][pos[1]] = student;
         }
 
+        int[] scoreTable = {0, 1, 10, 100, 1000};
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
                 int cnt = 0;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx >= 1 && nx < N+1 && ny >= 1 && ny < N+1) {
-                            for (int n : data[map[i][j]]) {
-                                if (map[nx][ny] == n) {
-                                    cnt++;
-                                }
+                for (int d = 0; d < 4; d++) {
+                    int nx = i + dx[d];
+                    int ny = j + dy[d];
+                    if (inRange(nx, ny)) {
+                        for (int like : data[map[i][j]]) {
+                            if (map[nx][ny] == like) {
+                                cnt++;
+                                break;
                             }
                         }
-
-
-
                     }
-                if(cnt == 1){
-                    answer += 1;
-                } else if (cnt == 2){
-                    answer += 10;
-                } else if (cnt == 3){
-                    answer += 100;
-                } else if (cnt == 4){
-                    answer += 1000;
                 }
-                }
-
-
-
+                answer += scoreTable[cnt];
+            }
         }
 
         System.out.println(answer);
     }
 
-
-    static void findLike(int student) {
-        int maxV = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (map[i][j] == 0) {
-                    int cnt = 0;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx >= 1 && nx < N+1 && ny >= 1 && ny < N+1) {
-                            for (int n : data[student]) {
-                                if (map[nx][ny] == n) {
-                                    cnt++;
-                                }
-                            }
-                        }
-                        maxV = Math.max(maxV, cnt);
-
-
-                    }
-                }
-            }
-
-        }
+    static List<int[]> findLike(int student) {
+        List<int[]> result = new ArrayList<>();
+        int maxLike = -1;
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (map[i][j] == 0) {
-                    int cnt = 0;
-                    for (int k = 0; k < 4; k++) {
-                        int nx = i + dx[k];
-                        int ny = j + dy[k];
-                        if (nx >= 1 && nx < N+1 && ny >= 1 && ny < N+1) {
-                            for (int n : data[student]) {
-                                if (map[nx][ny] == n) {
-                                    cnt++;
-                                }
+                if (map[i][j] != 0) continue;
+
+                int cnt = 0;
+                for (int d = 0; d < 4; d++) {
+                    int nx = i + dx[d], ny = j + dy[d];
+                    if (inRange(nx, ny)) {
+                        for (int like : data[student]) {
+                            if (map[nx][ny] == like) {
+                                cnt++;
+                                break;
                             }
                         }
-
-                    }
-                    if (cnt == maxV) {
-                        order1.add(new int[]{i, j});
                     }
                 }
 
+                if (cnt > maxLike) {
+                    result.clear();
+                    maxLike = cnt;
+                }
 
+                if (cnt == maxLike) {
+                    result.add(new int[]{i, j});
+                }
             }
-
-
         }
+        return result;
     }
 
+    static List<int[]> findMostEmpty(List<int[]> positions) {
+        List<int[]> result = new ArrayList<>();
+        int maxEmpty = -1;
 
-    static void emptyNum() {
-        int maxV = 0;
-        for(int[] now : order1){
-            int cnt=0;
-            int x = now[0];
-            int y = now[1];
-                for(int i=0; i<4; i++){
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-                    if(nx>=1 && nx<N+1 && ny>=1 && ny<N+1){
-                        if(map[nx][ny]==0){
-                            cnt++;
-                        }
-                    }
-                }
-                maxV = Math.max(maxV,cnt);
-        }
-        for(int[] now : order1){
-            int cnt=0;
-            int x = now[0];
-            int y = now[1];
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(nx>=1 && nx<N+1 && ny>=1 && ny<N+1){
-                    if(map[nx][ny]==0){
-                        cnt++;
-                    }
+        for (int[] pos : positions) {
+            int cnt = 0;
+            int x = pos[0], y = pos[1];
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d], ny = y + dy[d];
+                if (inRange(nx, ny) && map[nx][ny] == 0) {
+                    cnt++;
                 }
             }
-            if(cnt == maxV) {
-                order2.add(new int[] {x,y});
+
+            if (cnt > maxEmpty) {
+                result.clear();
+                maxEmpty = cnt;
             }
 
+            if (cnt == maxEmpty) {
+                result.add(new int[]{x, y});
+            }
         }
 
-
-
+        return result;
     }
 
+    static boolean inRange(int x, int y) {
+        return x >= 1 && x <= N && y >= 1 && y <= N;
+    }
 }
