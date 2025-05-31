7920cc0e7c2e23bac83c6ba4042040bd0a8cd937
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-05-31 15:49:40 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 236 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,93 +1,76 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
-	static int n;
-	static final int INF = Integer.MAX_VALUE;
-	static int[][] map;
-	static int[] dy = {-1, 0, 1, 0};
-	static int[] dx = {0, -1, 0, 1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int idx = 1;
-		while((n = Integer.parseInt(br.readLine())) != 0){
-			map = new int[n][n];
-			
-			for(int i = 0; i < n; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-				for(int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			int res = minCount();
-			
-			sb.append("Problem ").append(idx).append(":").append(" ").append(res).append("\n");
-			idx++;
-		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-	
-	static int minCount() {
-		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		boolean[][] visited = new boolean[n][n];
-		int[][] minCost = new int[n][n];
-		
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				minCost[i][j] = INF;
-			}
-		}
-		minCost[0][0] = map[0][0];
-		
-		pq.offer(new Pos(0, 0, map[0][0]));
-		
-		while(!pq.isEmpty()) {
-			Pos p = pq.poll();
-			int y = p.y;
-			int x = p.x;
-			int c = p.c;
-			
-			if(visited[y][x]) continue;
-			
-			visited[y][x] = true;
-			if(y == n - 1 && x == n-1) break;
-			
-			for(int d = 0; d < 4; d++) {
-				int ny = y + dy[d];
-				int nx = x + dx[d];
-				if(ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
-				if(visited[ny][nx]) continue;
-				if(minCost[ny][nx] <= c + map[ny][nx]) continue;
-				minCost[ny][nx] = c + map[ny][nx];
-				pq.offer(new Pos(ny, nx, minCost[ny][nx]));
-			}
-		}
-		return minCost[n-1][n-1];
-	}
-	
-	static class Pos implements Comparable<Pos>{
-		int y,x,c;
-		Pos(int y, int x, int c){
-			this.y = y;
-			this.x = x;
-			this.c = c;
-		}
-		@Override
-		public int compareTo(Pos o) {
-			return this.c - o.c;
-		}
-	}
+    static int N;
+    static int[][] map;
+    static int[][] dist;
+    static int[] dx ={-1,0,1,0};
+    static int[] dy ={0,1,0,-1};
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = 0;
+        while(true){
+            tc++;
+        N = Integer.parseInt(br.readLine());
+        if(N == 0) break;
+        map = new int[N][N];
+        dist = new int[N][N];
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++){
+                dist[i][j] = Integer.MAX_VALUE;
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int answer = dijkstar(0,0,N-1, N-1);
+        System.out.println("Problem "+tc+ ": " +answer);
+
+            }
+    }
+
+    static int dijkstar(int startX, int startY, int endX, int endY){
+
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]
+        ));
+        dist[startX][startY] = map[startX][startY];
+        pq.add(new int[] {startX,startY, dist[startX][startY]});
+
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int nowX = cur[0];
+            int nowY = cur[1];
+            int cost = cur[2];
+
+            if(dist[nowX][nowY] < cost) continue;
+
+            for(int i=0; i<4; i++){
+                int nx = nowX + dx[i];
+                int ny = nowY + dy[i];
+                if(nx>=0 && nx<N && ny>=0 && ny<N){
+                    if(dist[nx][ny] > map[nx][ny] + cost){
+                        dist[nx][ny] = map[nx][ny] + cost;
+                        pq.add(new int[] {nx,ny, dist[nx][ny]});
+                    }
+                }
+            }
+        }
+
+        return dist[endX][endY];
+
+
+    }
+
+
+
+
+
+
+
 }
