cbf740cb3dfd5440debdec6d29ff720da9a7ead4
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-06-03 20:20:14 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,12 +1,16 @@
-select case
-when (skill_code & (select sum(code) from skillcodes where category like 'FRONT%')) and skill_code & (select code from skillcodes where name = 'python') then 'A'
-when skill_code & (select code from skillcodes where name = 'C#') then 'B'
-when skill_code & (select sum(code) from skillcodes where category like 'FRONT%') then 'C'
-else NULL
-end as grade
-, id, email
-from developers
-group by grade, id, email
+# 프앤합 : 10256 파이썬 256 c# 1024 
+# A : Front End 스킬과 Python 스킬을 함께 가지고 있는 개발자
+# B : C# 스킬을 가진 개발자
+# C : 그 외의 Front End 개발자
+
+select 
+case
+    when skill_code & (select sum(code) from skillcodes where category like 'Front End') and skill_code & (select code from skillcodes where name like 'Python') then 'A'
+    when skill_code & (select code from skillcodes where name like 'C#') then 'B'
+    when skill_code & (select sum(code) from skillcodes where category like 'Front End') then 'C'
+end as grade,
+id, email
+from developers a
+group by grade,id,email
 having grade is not null
 order by grade, id
-
