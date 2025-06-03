e8b97b12bd589dcc4eb7d5818147d2962d9cd35e
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-06-03 23:55:44 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,6 @@
 select b.score, a.emp_no, a.emp_name, a.position, a.email
 from hr_employees a
-join (select emp_no, sum(score) as score
+join (select emp_no, sum(score) score
      from hr_grade
      group by emp_no
      order by sum(score) desc
