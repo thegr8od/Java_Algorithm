09f5c6f97fbab96b791e2f18575bb2c20e586c6f
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-06-04 16:15:30 +0900

    [level 2] Title: 구명보트, Time: 21.84 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 57.8 MB, 시간: 14.26 ms
+메모리: 75.5 MB, 시간: 21.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 10일 22:23:52
+2025년 06월 04일 16:15:30
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,19 +2,52 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people);
         int answer = 0;
+        boolean[] visited = new boolean[people.length];
+        Arrays.sort(people);
         int start = 0;
-        int end = people.length - 1;
+        int end = people.length-1;
+        int count =0;
         
-        while(start <= end){
-            if(people[start] + people[end] <= limit){
-                start++;
-            }
-            end--;
-            answer++;
+        if(people.length == 1){
+            return 1;
         }
         
-        return answer;
+        while(start<=end){
+            if(people[start] + people[end] <= limit) {
+                break;
+            }
+            else{
+                 end--;
+            }
+         
+        }
+        
+        System.out.println(end);
+        
+        while(start<end){
+            if(people[start] + people[end] <= limit){
+                visited[start] = true;
+                visited[end] = true;
+                start++;
+                end--;
+                count++;
+            }
+            else{
+                end--;
+            }
+        }
+        
+        
+        for(int i=0; i<visited.length; i++) {
+            if(!visited[i]) count++;
+        }
+        
+        System.out.println(count);
+        return count;
+        
+        
+        
+        
     }
 }
