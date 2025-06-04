867b8c0ca7c462c9b1b0f7af9c8419f37362661f
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-06-04 14:34:35 +0900

    [Gold IV] Title: 전화번호 목록, Time: 680 ms, Memory: 117140 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,44 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-    static int T,N;
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static int T, N;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
-        for(int tc=0; tc<T; tc++){
+
+        for (int tc = 0; tc < T; tc++) {
             N = Integer.parseInt(br.readLine());
             Set<String> set = new HashSet<>();
-            for(int i=0; i<N; i++){
+
+            for (int i = 0; i < N; i++) {
                 set.add(br.readLine());
             }
-            
 
             boolean flag = false;
 
-            for(String key : set){
-                String now = "";
-                for(int j=0; j<key.length(); j++){
-                    now += key.charAt(j);
-                    if(!now.equals(key) && set.contains(now)) {
+            for (String key : set) {
+                StringBuilder sb = new StringBuilder();
+
+                for (int j = 0; j < key.length(); j++) {
+                    sb.append(key.charAt(j));
+                    String now = sb.toString();
+
+                    if (!now.equals(key) && set.contains(now)) {
                         flag = true;
+                        break;
                     }
                 }
+
+                if (flag) break; // 접두어 하나라도 발견 시 전체 루프 종료
             }
 
-            if(flag) {
-                System.out.println("NO");
-            } else{
-                System.out.println("YES");
-            }
-
+            System.out.println(flag ? "NO" : "YES");
         }
-
-
-
-
     }
-
-
 }
