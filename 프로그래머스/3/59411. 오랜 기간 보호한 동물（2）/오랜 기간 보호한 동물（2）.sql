6da0896e2bef597de19d216f5a1f5d11c2e9474b
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-06-05 02:11:18 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,7 +1,6 @@
--- 코드를 입력하세요
-SELECT o.animal_id, o.name
-FROM animal_outs o
-JOIN animal_ins i
-ON o.animal_id = i.animal_id
-order by datediff(o.datetime, i.datetime) desc
+select a.animal_id, a.name
+from animal_ins a
+join animal_outs b
+on a.animal_id = b.animal_id
+order by datediff(b.datetime, a.datetime) desc
 limit 2
