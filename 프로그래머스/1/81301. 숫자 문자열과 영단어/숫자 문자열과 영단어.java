915d24880db629e0dd658203d19ef0c290ee4399
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-07-03 21:41:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 14.21 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,25 +1,6 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String ans = "";
-        String temp = "";
-        String[] numbers = {"zero","one","two","three","four","five","six","seven","eight","nine"};
-        for(int i=0; i<s.length(); i++){
-            if(Character.isDigit(s.charAt(i))){
-                ans += s.charAt(i);
-            }
-            else{
-                temp += s.charAt(i);
-            }
-            for(int j=0; j<10; j++){
-                    if(temp.equals(numbers[j])){
-                        ans += j;
-                        temp = "";
-                    }
-                
-            }
-        }
-        answer = Integer.parseInt(ans);
         return answer;
     }
 }
