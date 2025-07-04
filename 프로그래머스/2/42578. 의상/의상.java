8ba3717b6ea1eb306a17ed5101f7dbfdce5abd12
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-07-04 22:33:16 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,21 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
+        int answer = 0;
         Map<String, Integer> map = new HashMap<>();
         
-        for(String[] str : clothes){
-            map.put(str[1], map.getOrDefault(str[1],0)+1);
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0)+1);
         }
+        int now = 1;
+        for(String cloth : map.keySet()){
+            now *= (map.get(cloth)+1);
+        }
+        now -= 1;
         
-        for(String key : map.keySet()){
-            System.out.print(map.get(key));
-        }
-        
-        for(String key : map.keySet()){
-            answer *= (map.get(key)+1);
-        }
-        answer -= 1;
-        return answer;
+        return now;
     }
 }
