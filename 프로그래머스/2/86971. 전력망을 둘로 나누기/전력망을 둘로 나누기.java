d3e28559bade9d825fe40ad91a7434a979e2e1cd
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-07-04 14:09:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.85 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,60 @@
 import java.util.*;
 
 class Solution {
+    static List<List<Integer>> lst;
+    static boolean[] visited;
+    static int d;
     public int solution(int n, int[][] wires) {
-        int answer = 9999999;
-        List<List<Integer>> lst = new ArrayList<>();
+        int answer = Integer.MAX_VALUE;
         
+        lst = new ArrayList<>();
         for(int i=0; i<=n; i++){
             lst.add(new ArrayList<>());
         }
         
-        for (int[] wire : wires){
+    
+        for(int[] wire : wires){
             int a = wire[0];
             int b = wire[1];
             lst.get(a).add(b);
             lst.get(b).add(a);
         }
         
-        for (int[] wire : wires){
+        for(int[] wire: wires){
             int a = wire[0];
             int b = wire[1];
+            
             lst.get(a).remove(Integer.valueOf(b));
             lst.get(b).remove(Integer.valueOf(a));
-            boolean[] visited = new boolean[n+1];
-            int first = dfs(a, lst, visited);
-            int second = n - first;
             
-            answer = Math.min(answer, Math.abs(first-second));
+            visited = new boolean[n+1];
+            
+            d = 0;
+            dfs(a);
+            int d1 = d;
+            
+            
+            d = 0;
+            dfs(b);
+            int d2 = d;
+            
+            answer = Math.min(answer, Math.abs(d1-d2));
+            
             lst.get(a).add(b);
             lst.get(b).add(a);
         }
-        
         return answer;
     }
     
-    public int dfs(int idx, List<List<Integer>> lst, boolean[] visited){
-        visited[idx] = true;
-        int cnt = 1;
+    public void dfs(int node){
+       visited[node] = true;
+        d++;
         
-        
-        for(int next : lst.get(idx)){
-            if(!visited[next]){
-                cnt += dfs(next,lst,visited);
+        for (int next : lst.get(node)){
+            if(!visited[next]) {
+                dfs(next);
             }
         }
         
-        
-        return cnt;
     }
 }
