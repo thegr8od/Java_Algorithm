1ce1ef99aedf5c05eab8facb4f66c008d88d34d1
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-07-04 16:26:55 +0900

    [level 2] Title: H-Index, Time: 7.50 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,30 +3,24 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        // n편 중, h번 이상 인용된 논문이 h편 이상, 나머지 논문이 h번 이하 인용
-        // h의 최댓값 구하기
+
         Arrays.sort(citations);
+
         int n = 0;
-        while(true){
+        while (true) {
             int count = 0;
-            for(int i=citations.length-1; i>=0; i--){
-                if(citations[i] >= n){
+            for (int i = 0; i < citations.length; i++) {
+                if (citations[i] >= n) {
                     count++;
                 }
-                else{
-                    break;
-                }
             }
-            if(count>=n && citations.length - count <= n){
+            if (count >= n && citations.length - count <= n) {
                 answer = Math.max(answer, n);
                 n++;
-            }
-            else{
+            } else {
                 break;
             }
-            
         }
-        
         return answer;
     }
 }
