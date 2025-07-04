88f513cf11d414584e9510011f3034ac84aacc0f
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-07-04 14:09:24 +0900

    [level 2] Title: 피로도, Time: 3.46 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,23 @@
 class Solution {
-    static int answer;
     static boolean[] visited;
+    static int answer = -1;
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0);
-        
+        dfs(0,dungeons, k);
         return answer;
     }
     
-    public static void dfs (int k, int[][] dungeons, int depth){
+    public void dfs(int d, int[][] dungeons, int k){
         
-        answer = Math.max(answer, depth);
+        answer = Math.max(answer,d);
+        
         
         for(int i=0; i<dungeons.length; i++){
-            if(!visited[i] && k>= dungeons[i][0]){
+            if(!visited[i] && k>=dungeons[i][0]){
                 visited[i] = true;
-                dfs(k-dungeons[i][1], dungeons, depth+1);
-                visited[i]= false;
+                dfs(d+1,dungeons, k-dungeons[i][1]);
+                visited[i] = false;
             }
         }
-        
-        
     }
 }
