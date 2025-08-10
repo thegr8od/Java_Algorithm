4d8bda40ffdc5afad1141da6d8e5d1281584dadf
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-08-10 18:10:20 +0900

    [level 1] Title: 모의고사, Time: 0.53 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,37 +3,37 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
-        
-        int oneIdx = 0;
-        int twoIdx = 0;
-        int threeIdx = 0;
+        int[] one = {1,2,3,4,5}; //5
+        int[] two = {2,1,2,3,2,4,2,5}; //8
+        int[] three = {3,3,1,1,2,2,4,4,5,5}; //10
         
         int oneC = 0;
         int twoC = 0;
         int threeC = 0;
-        for(int num : answers){
-            if(one[oneIdx++ % 5] == num) oneC++;
-            if(two[twoIdx++ % 8] == num) twoC++;
-            if(three[threeIdx++ % 10] == num) threeC++;
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == one[i%5]) oneC++;
+            if(answers[i] == two[i%8]) twoC++;
+            if(answers[i] == three[i%10]) threeC++;
         }
         
         int maxC = Math.max(oneC, Math.max(twoC, threeC));
+        
         List<Integer> lst = new ArrayList<>();
         
-        if(maxC == oneC) lst.add(1);
-        if(maxC == twoC) lst.add(2);
-        if(maxC == threeC) lst.add(3);
-        
-        
+        if(oneC == maxC) lst.add(1);
+        if(twoC == maxC) lst.add(2);
+        if(threeC == maxC) lst.add(3);
         
         int[] answer = new int[lst.size()];
         
-        for(int i=0; i<answer.length; i++){
+        for(int i=0; i<lst.size(); i++){
             answer[i] = lst.get(i);
         }
+        
+        
+        
+        
         return answer;
     }
 }
