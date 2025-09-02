872bd9580c3ba40ef5f38055ff7e596b2e4c92b0
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-02 13:55:50 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,27 +1,24 @@
-import java.util.*;
-
 class Solution {
     public int solution(String name) {
-        int len = name.length();
         int answer = 0;
+        int length = name.length();
         
-        for (int i =0; i< len; i++){
-            char ch = name.charAt(i);
-            answer += Math.min(ch - 'A', 'Z' - ch + 1);
-        }
+        int index;
+        int move = length - 1;
         
-        int move = len - 1;
-        
-        for (int i=0; i< len; i++){
-            int next = i + 1;
-            while (next < len && name.charAt(next) =='A'){
-                next++;
+        for(int i=0; i<name.length(); i++){
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+            
+            index = i+1;
+            
+            while(index < length && name.charAt(index) == 'A'){
+                index++;
             }
             
-            // 현재 위치까지 갔다가 돌아와서 끝까지 가는 경우
-            move = Math.min(move, i*2+len - next);
-            // 반대로 끝까지 갔다가 돌아와서 i까지 가는 경우
-            move = Math.min(move, (len - next) * 2 + i);
+            // 순서대로 가는 것과, 뒤로 돌아가는 것 중 이도수가 적은 것을 선택
+            move = Math.min(move, i*2 + length - index);
+            // 처음부터 뒷부분을 먼저 입력하는 것이 더 빠른 경우까지 고려
+            move = Math.min(move, (length-index) * 2 + i);
         }
         
         return answer + move;
