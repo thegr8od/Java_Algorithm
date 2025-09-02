1c82fb45c38cc02b56adfa53e1fe4ed4f8c93acb
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-02 11:13:09 +0900

    [level 1] Title: 체육복, Time: 0.67 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,30 +3,42 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;
-        
         Arrays.sort(lost);
         Arrays.sort(reserve);
+        boolean[] visited = new boolean[reserve.length];
+        int rec = 0;
         
-        for(int i =0; i< lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
+        // 앞사람부터
+        List<Integer> lst = new ArrayList<>();
+        for(int num : reserve){
+            lst.add(num);
         }
         
         for(int i=0; i<lost.length; i++){
             for(int j=0; j<reserve.length; j++){
-                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
-                    answer++;
-                    reserve[j] = -1;
-                    break;
+                if(lost[i] == reserve[j]){
+                    visited[j] = true;
+                    rec++;
+                    lost[i] = -1;
                 }
             }
         }
-       return answer;
+        
+        for(int num : lost){
+            if(num != -1){
+            for(int j=0; j<reserve.length; j++){
+                if(!visited[j] && Math.abs(num - reserve[j]) == 1){
+                    visited[j] = true;
+                    rec++;
+                    break;
+                }
+            }
+            }
+        }
+        
+        answer += rec;
+        
+        
+        return answer;
     }
 }
