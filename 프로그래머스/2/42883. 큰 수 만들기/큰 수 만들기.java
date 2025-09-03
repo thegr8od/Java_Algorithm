086d9799e3330baf521d205fc86cd70fb83476e5
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-03 13:50:20 +0900

    [level 2] Title: 큰 수 만들기, Time: 7223.09 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,21 +1,18 @@
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder answer = new StringBuilder("");
-        int len = number.length() - k;
-        int start = 0;
-        
-        while (start<number.length() && answer.length() != len){
-            int leftNum = k + answer.length() + 1;
-            int max = 0;
-            for (int j= start; j <leftNum; j++){
-                if (max < number.charAt(j) - '0'){
-                    max = number.charAt(j) - '0';
-                    start = j + 1;
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        int max = 0;
+        for(int i=0; i<number.length() - k; i++) {
+            max = 0;
+            for(int j = index; j<= k+i; j++) {
+                if(max < number.charAt(j)-'0') {
+                    max = number.charAt(j)-'0';
+                    index = j+1;
                 }
             }
-            answer.append(Integer.toString(max));
+            sb.append(max);
         }
-        
-        return answer.toString();
+        return sb.toString();
     }
 }
