6599bc97a504abe18544cf7bcb269a7b425560c7
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-04 13:43:56 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,22 +4,23 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         int now = 0;
-        for(int[] command : commands){
-            int start = command[0]-1;
-            int end = command[1];
-            int idx = command[2]-1;
+        for(int[] cmd : commands){
+            int start = cmd[0]-1;
+            int end = cmd[1]-1;
+            int index = cmd[2]-1;
+            
             List<Integer> lst = new ArrayList<>();
             
-            for(int i = start; i<end; i++){
+            for(int i=start; i<=end; i++){
                 lst.add(array[i]);
             }
             
             Collections.sort(lst);
-            
-            answer[now++] = lst.get(idx);
+            answer[now++] = lst.get(index);            
             
             
         }
+        
         return answer;
     }
 }
