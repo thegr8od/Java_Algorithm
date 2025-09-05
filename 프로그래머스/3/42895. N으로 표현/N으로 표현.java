f2f82cf718f2f41dbc8a7d887fe009dc6564448e
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-05 11:14:39 +0900

    [level 3] Title: N으로 표현, Time: 25.78 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,31 +2,47 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        
-       if (N==number) return 1;
+        if (N== number){
+            return 1;
+        }
         
         List<Set<Integer>> dp = new ArrayList<>();
-        for(int i =0; i<= 8; i++){
+        for(int i=0; i<=8; i++){
             dp.add(new HashSet<>());
         }
         
-        for (int i =1; i<=8; i++){
-            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp.get(i).add(repeated);
+        dp.get(1).add(N);
+        
+        for(int i=2; i<9; i++){
+            
+            StringBuilder sb = new StringBuilder();
+            
+            for(int j=0; j<i; j++){
+                sb.append(N);
+            }
+            
+            dp.get(i).add(Integer.parseInt(sb.toString()));
             
             for(int j=1; j<i; j++){
-                for (int op1 : dp.get(j)){
-                    for (int op2: dp.get(i-j)){
-                        dp.get(i).add(op1+op2);
-                        dp.get(i).add(op1 -op2);
-                        dp.get(i).add(op1 * op2);
-                        if (op2 !=0) dp.get(i).add(op1 / op2);
+                int k = i-j;
+                for(int num1 : dp.get(j)) {
+                    for (int num2 : dp.get(k)){
+                        dp.get(i).add(num1 + num2);
+                        dp.get(i).add(num1 - num2);
+                        dp.get(i).add(num1 * num2);
+                        if(num2 != 0){
+                            dp.get(i).add(num1 / num2);
+                        }
                     }
                 }
             }
-            
-            if (dp.get(i).contains(number)) return i;
-        }
+                
+                if(dp.get(i).contains(number)) {
+                    return i;
+                }
+            }
+        
+        
         return -1;
     }
 }
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 33.90 ms
+메모리: 85 MB, 시간: 25.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 25일 23:13:36
+2025년 09월 05일 11:14:36
 
 ### 문제 설명
 
