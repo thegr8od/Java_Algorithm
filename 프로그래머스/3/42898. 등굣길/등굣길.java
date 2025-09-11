58cfdba95636d8c368cbc4df31cea09dadfc76e3
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-11 14:39:29 +0900

    [level 3] Title: 등굣길, Time: 1.09 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,29 +1,22 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n+1][m+1];
+        dp[1][1] = 1;
         int mod = 1000000007;
         
-        for(int[] temp : puddles){
-            dp[temp[1]][temp[0]] = -1;
+        for(int[] puddle : puddles){
+            dp[puddle[1]][puddle[0]] = -1;
         }
         
-        dp[1][1] = 1;
-        
-        for(int i =1; i<n+1; i++){
-            for(int j =1; j<m+1; j++){
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
                 if(dp[i][j] == -1) continue;
                 
-                if(dp[i-1][j] > 0){
-                    dp[i][j] += dp[i-1][j];
-                    dp[i][j] %= mod;
-                }
-                if(dp[i][j-1] > 0){
-                    dp[i][j] += dp[i][j-1];
-                    dp[i][j] %= mod;
-                }
+                if(dp[i-1][j] >0) dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
+                if(dp[i][j-1] >0) dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
             }
         }
-        
-        return dp[n][m];
+        int answer = dp[n][m];
+        return answer;
     }
 }
