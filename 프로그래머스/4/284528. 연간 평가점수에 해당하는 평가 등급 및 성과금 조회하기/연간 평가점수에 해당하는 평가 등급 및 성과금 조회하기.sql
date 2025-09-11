157e8349ede5397be2badfaef6151a39857f0e27
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-11 14:51:57 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,16 @@
-select a.emp_no, a.emp_name, case
-when b.avg >= 96 then 'S'
-when b.avg >= 90 then 'A'
-when b.avg >= 80 then 'B'
-else 'C'
-end as grade, 
-case
-when b.avg >= 96 then sal * 0.2
-when b.avg >= 90 then sal * 0.15
-when b.avg >= 80 then sal * 0.1
+select a.emp_no, a.emp_name, b.grade, case
+when b.grade like 'S' then a.sal * 0.2
+when b.grade like 'A' then a.sal * 0.15
+when b.grade like 'B' then a.sal * 0.1
 else 0
 end as bonus
 from hr_employees a
-join (select emp_no, avg(score) as avg
-     from hr_grade
-     group by emp_no) b
+join (select emp_no, case
+when avg(score) >= 96 then 'S'
+when avg(score) >= 90 then 'A'
+when avg(score) >= 80 then 'B'
+else 'C' end as grade
+from hr_grade
+group by emp_no) b
 on a.emp_no = b.emp_no
-order by a.emp_no
+order by emp_no
