77f18483dd43fb0a1039366141fef04c55b5ffb0
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-15 16:47:59 +0900

    [level 4] Title: 사칙연산, Time: 2.18 ms, Memory: 54.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(String arr[]) {
+        int max = 0;
+        int min = 0;
+        int sum = 0;
+        
+        for(int i=0; i<arr.length-1; i+= 2){
+            int num = Integer.parseInt(arr[arr.length -1 -i]);
+            
+            String pm = arr[arr.length - 2 - i];
+            
+            if(pm.equals("+")) sum += num;
+            
+            else if (pm.equals("-")){
+                int temp1 = -(num + sum + min);
+                int temp2 = -num + sum + max;
+                int temp3 = -(num +sum +max);
+                int temp4 = -(num+sum) + min;
+                max = Math.max(temp1, temp2);
+                min = Math.min(temp3, temp4);
+                sum = 0;
+            }
+        }
+        return max + Integer.parseInt(arr[0]) + sum;
+    }
+}
