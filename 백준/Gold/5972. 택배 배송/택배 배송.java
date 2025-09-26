ae9b72cf346dd769b40f009777f4eda8c366d12f
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-09-26 13:51:57 +0900

    [Gold V] Title: 택배 배송, Time: 416 ms, Memory: 41704 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -4,61 +4,67 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N,M;
+    static int[][] map;
+    static int n,m;
     static int maxV = 1000000000;
-    static StringTokenizer st;
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
     static int[] dist;
     static List<int[]>[] graph;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st= new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList[N+1];
-        dist = new int[N+1];
-
-        for(int i=1; i<=N; i++){
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[n+1];
+        dist = new int[n+1];
+        for(int i=0; i<n+1; i++){
             graph[i] = new ArrayList<>();
             dist[i] = maxV;
         }
 
-        for(int i=0; i<M; i++){
+        for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            graph[a].add(new int[]{b,c});
-            graph[b].add(new int[]{a,c});
+
+            graph[a].add(new int[] {b,c});
+            graph[b].add(new int[] {a,c});
+
         }
 
-        System.out.println(dijkstar(1, N));
+        System.out.println(dijkstar(1, n));
 
     }
 
-    static int dijkstar(int start, int end ) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
-        dist[start] = 0;
-        pq.offer(new int[]{start, 0});
+    static int dijkstar(int start, int end){
 
-        while (!pq.isEmpty()) {
+        PriorityQueue<int []> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1], b[1]));
+        dist[start] = 0;
+        pq.add(new int[] {start, dist[start]});
+
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
             int now = cur[0];
             int time = cur[1];
 
-            if (dist[now] < time) continue;
+            if(time > dist[now]) continue;
 
-            for (int[] next : graph[now]) {
-                int to = next[0];
-                int cost = next[1];
-                if (dist[to] > dist[now] + cost) {
+            for(int[] num : graph[now]){
+                int to = num[0];
+                int cost = num[1];
+
+                if(dist[to] > dist[now] + cost){
                     dist[to] = dist[now] + cost;
-                    pq.offer(new int[]{to, dist[to]});
+                    pq.add(new int[]{to, dist[to]});
                 }
             }
-
         }
+
+
+
         return dist[end];
     }
 
