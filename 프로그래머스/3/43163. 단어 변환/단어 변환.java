881d8e7eefb48d17ad409ebea2f6829fc92c6d1c
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-10-30 13:35:19 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,48 @@
 import java.util.*;
 
-
 class Solution {
+    static int cnt;
     static boolean[] visited;
-    static int answer = Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        cnt = words.length;
         visited = new boolean[words.length];
         dfs(begin, target, words, 0);
         
-        answer = answer == Integer.MAX_VALUE  ? 0 : answer;
+        if(cnt == words.length){
+            answer = 0;
+        } else{
+            answer = cnt;
+        }
         return answer;
     }
+    
     
     static void dfs(String now, String target, String[] words, int d){
         
         
         
         if(now.equals(target)){
-            answer = Math.min(answer,d);
+            cnt = Math.min(cnt,d);
             return;
         }
         
-        
         for(int i=0; i<words.length; i++){
-            int cnt = 0;
-            for(int j=0; j<now.length(); j++){
-                if(now.charAt(j)==words[i].charAt(j)) cnt++;
-                if(cnt == now.length()-1 && !visited[i]){
+            if(!visited[i]){
+                int cor = 0;
+                for(int j=0; j<words[i].length(); j++){
+                    if(now.charAt(j) == words[i].charAt(j)){
+                        cor++;
+                    }
+                }
+                if(words[i].length() - cor == 1){
                     visited[i] = true;
                     dfs(words[i], target, words, d+1);
                     visited[i] = false;
                     
                 }
             }
-            
         }
+        
     }
 }
