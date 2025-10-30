2c7c2ba77c20bff1406be31c3755362f5c54565d
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-10-30 16:32:37 +0900

    [level 2] Title: 양궁대회, Time: 8405.02 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,47 +1,6 @@
 class Solution {
-    static int[] res = new int[11]; //점수차 최대일때
-    static int[] lion = {-1};
-    static int max = Integer.MIN_VALUE;
-    
     public int[] solution(int n, int[] info) {
-        back(0,n,info);
-        
-        if(max==-1){
-            lion = new int[1];
-            lion[0] = -1;
-        }
-        
-        return lion;
-    }
-    
-    static void back(int depth, int n, int[] info){
-        if(depth==n){
-            int diff = score(info);
-            if(max<=diff){
-                max = diff;
-                lion = res.clone();
-            }
-            return;
-        }
-        
-        for(int i=0; i<info.length && res[i]<=info[i]; i++){
-            res[i] += 1;
-            back(depth+1, n, info);
-            res[i] -= 1;
-        }
-        
-    }
-    
-    static int score(int[] info){
-        int apeach=0, lion=0;
-        for(int i=0; i<res.length; i++){
-            if(info[i] ==0 && res[i] == 0) continue;
-            if(info[i]>=res[i]) apeach += (10-i);
-            else lion += (10-i);
-        }
-        
-        int diff = lion - apeach;
-        if(diff<=0) return -1;
-        return diff;
+        int[] answer = {};
+        return answer;
     }
 }
