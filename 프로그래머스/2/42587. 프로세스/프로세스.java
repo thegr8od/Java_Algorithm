dc27335da81cf262741b98be083640be68197689
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-11-03 20:47:18 +0900

    [level 2] Title: 프로세스, Time: 1.04 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
-        for (int i : priorities){
-            pq.offer(i);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int number : priorities){
+            pq.add(number);
         }
         
+        
         while(!pq.isEmpty()){
+            
             for(int i=0; i<priorities.length; i++){
                 if(pq.peek() == priorities[i]){
                     pq.poll();
                     answer++;
-                    
-                    if(i == location) {
+                    if(i == location){
                         return answer;
                     }
                 }
             }
         }
-    
-        
-        
         
         return answer;
     }
