da8ada05772c39d6b12b5a369cc88977c1148cef
Seungjun Kim <127323247+thegr8od@users.noreply.github.com>
2025-11-04 17:53:04 +0900

    [level 2] Title: 구명보트, Time: 15.08 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,51 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        boolean[] visited = new boolean[people.length];
+        
         Arrays.sort(people);
         int start = 0;
-        int end = people.length-1;
-        int count =0;
-        
-        if(people.length == 1){
-            return 1;
-        }
-        
-        while(start<=end){
-            if(people[start] + people[end] <= limit) {
-                break;
-            }
-            else{
-                 end--;
-            }
-         
-        }
-        
-        System.out.println(end);
-        
+        int end = people.length - 1;
+        int cnt = 0;
         while(start<end){
             if(people[start] + people[end] <= limit){
-                visited[start] = true;
-                visited[end] = true;
+                cnt++;
                 start++;
                 end--;
-                count++;
             }
             else{
                 end--;
             }
+            
         }
-        
-        
-        for(int i=0; i<visited.length; i++) {
-            if(!visited[i]) count++;
-        }
-        
-        System.out.println(count);
-        return count;
-        
-        
-        
-        
+        return people.length - cnt;
     }
 }
